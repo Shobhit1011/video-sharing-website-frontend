f97470072d7c2a68fb85258b745697a6463cbe0f
shobhit <shobhitcoolsinghal@gmail.com>
2022-01-02 19:14:09 +0530

    Resolved migration issues

--- src/app/video-play/video-play.component.ts
+++ src/app/video-play/video-play.component.ts
@@ -43,7 +43,7 @@ export class VideoPlayComponent implements OnInit {
     private elRef: ElementRef, public dialog: MatDialog, private http: HttpClient, private router: Router, 
     private ratingService: RatingService, private loginService: LoginService, private toastr: ToastrService, 
     private titleService: Title, private websocketAPI: WebSocketService, private subscriptionService: SubscriptionService) {
-      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  
   }
 
   @HostListener('window:resize', ['$event'])
@@ -105,6 +105,7 @@ export class VideoPlayComponent implements OnInit {
   }
 
   ngOnInit() {
+    console.log("Insidr ittititti")
     this.widthCalculation();
     this.loadVideoScript();
     document.getElementById('cover-spin').style.display = "none";
